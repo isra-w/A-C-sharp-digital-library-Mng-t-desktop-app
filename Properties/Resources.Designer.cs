c317fdcc1090d85ffafec7c6f4da3adb5b836904
Esrael <israeyob1995@gmail.com>
2026-05-24 23:57:18 +0300

    updated project an dform name

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace d_labdemo.Properties {
+namespace D_lab.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace d_labdemo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("d_labdemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("D_lab.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
